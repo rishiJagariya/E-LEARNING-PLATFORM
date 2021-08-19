d7c385565d7a6824ea6f7aefe8b2f25009860e45
Rishi jagariya <43576913+rishiJagariya@users.noreply.github.com>
2021-08-19 11:55:16 +0530

    temp commit only

---
+++ backend/src/main/java/com/elp/session/LoginServlet.java
@@ -0,0 +1,65 @@
+package com.elp.session;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.elp.service.StudentService;
+import com.elp.service.TrainerService;
+
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	@Autowired
+	StudentService studentService;
+  
+	@Autowired
+	TrainerService trainerService;
+	
+    public LoginServlet() { super(); }
+
+	
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+    	String username = request.getParameter("username");
+    	String password = request.getParameter("passsword");
+    	String userType = request.getParameter("usertype");
+    	
+    	String message;
+        if (userType == "trainer") {
+            Trainer trainer = trainerService.getTrainerByUsername(username);
+            
+            if (trainer == null) {
+                message = "Username is incorrect";
+                
+            } else if (trainer.getPassword() == password) {
+                message = "User logged in successfully";
+                
+            } else {
+                message = "Password is incorrect";
+            }
+        } else if (userType == "student") {
+            Student student = studentService.getStudentByUsername(username);
+            
+            if (student == null) {
+                message = "Username is incorrect";
+                
+            } else if (student.getPassword() == password) {
+                message = "User logged in successfully";
+                
+            } else {
+                message = "Password is incorrect";
+            }
+        }
+    	
+		doGet(request, response);
+	}
+
+}
